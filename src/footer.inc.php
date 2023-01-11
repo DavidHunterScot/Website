e812268b8d79321f979102a07d394a2156249d7d
DavidHunterScot <contact@davidhunter.scot>
2023-01-11 17:26:08 +0000

    A few tweaks and added favicon

--- src/footer.inc.php
+++ src/footer.inc.php
@@ -5,15 +5,6 @@
             </div>
         </footer>
 
-        <script>
-            var email_addresses = document.getElementsByClassName( 'email_address' );
-
-            for( let i = 0; i < email_addresses.length; i++ )
-            {
-                var the_email_address = email_addresses[ i ].innerHTML.replaceAll( ' [at] ', '@' ).replaceAll( ' [dot] ', '.' );
-
-                email_addresses[ i ].innerHTML = '<a href="mailto:' + the_email_address + '" target="_blank">' + the_email_address + '</a>';
-            }
-        </script>
+        <script src="/assets/js/anti-email-harvesting.js"></script>
     </body>
 </html>
--- src/index.php
+++ src/index.php
@@ -2,8 +2,8 @@
 
 <section class="w3-ios-dark-blue w3-padding-64">
     <div class="w3-auto w3-padding w3-padding-64">
-        <h2 class="w3-center"><b>Hello, I'm David Hunter!</b></h2>
-        <p class="w3-center"><i>Internet Services Specialist from Ayrshire, Scotland.</i></p>
+        <h2 class="w3-center w3-xxxlarge">Hello, I'm <b>David Hunter</b>!</h2>
+        <p class="w3-center w3-large"><i>Internet Services Specialist from Ayrshire, Scotland.</i></p>
         <h3 class="w3-center"><b>Under Construction</b></h3>
         <p class="w3-center">Reach out via email: <b class="email_address">contact [at] davidhunter [dot] scot</b></p>
     </div>
